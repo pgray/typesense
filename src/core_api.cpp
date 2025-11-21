ca7120000f8f60ba86be679cfae0966b91f9f316
Patrick Gray <patrick@baselayer.com>
2025-11-20 20:42:16 -0800

    ...

--- src/core_api.cpp
+++ src/core_api.cpp
@@ -524,7 +524,8 @@ bool post_health(const std::shared_ptr<http_req>& req, const std::shared_ptr<htt
             res->set_body(400, result.dump());
             return alive;
         }
-        if(req_json.contains("shutdown"){ //TODO: && req_json["shutdown"] == true) {
+        // TODO: && req_json["shutdown"] == true)
+        if(req_json.contains("shutdown")) {
             server->set_unhealthy();
             result["ok"] = false;
             result["msg"] = "Shutdown requested.";
